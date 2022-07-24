29061a36a10c33d344a5763423658e12c08f7120
Paulo Vieira <45016641+pcvp@users.noreply.github.com>
2022-07-24 19:29:03 -0300

    Home Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header";
+
+export function Home() {
+  return (
+    <div className="bg-gray-700 min-h-screen">
+      <Header />
+    </div>
+  );
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 import { Share } from "./pages/Share";
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/share/:slug" element={<Share />} />
     </Routes>
   );
